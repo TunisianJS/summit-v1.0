4f74f31dfcfc3d4cd99ecf9778b1604b88c991e4
3imed-jaberi <imed_jebari@hotmail.fr>
2021-12-03 23:52:24 +0100

    fix speachs lang

--- src/components/Homepage/AllState.js
+++ src/components/Homepage/AllState.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const abdennour =  require('../Common/images/abdennour.jpg');
+const abdennour = require('../Common/images/abdennour.jpg');
 
 const aymen = require('../Common/images/aymen.jpg');
 
@@ -19,10 +19,10 @@ const speakersDetails = [
     speakersImg: abdennour,
     speakersDesignation: (
       <span>
-          In-mem sub/pub with Mufa,
-          <br />
-          Site Reliability Engineer at ELM
-        </span>
+        In-mem sub/pub with Mufa,
+        <br />
+        Site Reliability Engineer at ELM
+      </span>
     ),
   },
   {
@@ -31,10 +31,10 @@ const speakersDetails = [
     speakersImg: aymen,
     speakersDesignation: (
       <span>
-      Nest.js the trendy Node.js Framework
-      <br />
-      Teacher at INSAT
-    </span>
+        Nest.js the trendy Node.js Framework
+        <br />
+        Teacher at INSAT
+      </span>
     ),
   },
   {
@@ -43,10 +43,10 @@ const speakersDetails = [
     speakersImg: burawi,
     speakersDesignation: (
       <span>
-  An Arabic version of Javascript. Why ?
-  <br />
-  Senior Full Stack Developer at Think-it
-</span>
+        An Arabic version of Javascript. Why ?
+        <br />
+        Senior Full Stack Developer at Think-it
+      </span>
     ),
   },
   {
@@ -79,10 +79,10 @@ const speakersDetails = [
     speakersImg: nacer,
     speakersDesignation: (
       <span>
-      SOLID principles between theory and practice in JavaScript.
-      <br />
-      Senior FrontEnd Engineer at Oodrive
-    </span>
+        SOLID principles between theory and practice in JavaScript.
+        <br />
+        Senior FrontEnd Engineer at Oodrive
+      </span>
     ),
   },
   /*
@@ -102,9 +102,9 @@ const talkDetailsPage = [
     title: 'In-mem sub/pub with Mufa',
     speakersTitle: 'About the speakers',
     backLink: '/talks/',
-    language: 'English',
+    language: 'Arabic',
     time: '08:00 am -	08:30 am',
-    talkType: 'Fireside Chat',
+    talkType: '',
     description: [
       {
         desc: 'Coming Soon',
@@ -119,10 +119,10 @@ const talkDetailsPage = [
         speakersBio: '-',
         speakersDesignation: (
           <span>
-          In-mem sub/pub with Mufa,
-          <br />
-          Site Reliability Engineer at ELM
-        </span>
+            In-mem sub/pub with Mufa,
+            <br />
+            Site Reliability Engineer at ELM
+          </span>
         ),
       },
     ],
@@ -138,9 +138,9 @@ const talkDetailsPage = [
     title: 'Nest.js the trendy Node.js Framework.',
     speakersTitle: 'About the speakers',
     backLink: '/talks/',
-    language: 'English',
+    language: 'Arabic',
     time: '08:00 am -	08:30 am',
-    talkType: 'Fireside Chat',
+    talkType: '',
     description: [
       {
         desc: 'Coming Soon',
@@ -155,158 +155,158 @@ const talkDetailsPage = [
         speakersBio: 'He has a Ph.D. in Computer Science since January 2014 from the University of Strasbourg and the Faculty of Sciences of Tunis. He has been teaching at INSAT since 2013. Also, He is a consultant, doing professional training for 4 years on Symfony Angular and Nest.js and a content creator, co-creator of the Techwall YT channel. He coached the INSAT problem-solving teams where they won the last 4 TCPCs by participating in the ACPC and being the first Tunisian team to participate in the ICPC.',
         speakersDesignation: (
           <span>
-          Nest.js the trendy Node.js Framework
-          <br />
-          Teacher at INSAT
-        </span>
+            Nest.js the trendy Node.js Framework
+            <br />
+            Teacher at INSAT
+          </span>
         ),
       },
     ],
   },
-{
-  url: '/talks/an-arabic-version-of-javascript/',
-  metaTags: {
+  {
+    url: '/talks/an-arabic-version-of-javascript/',
+    metaTags: {
+      title: 'An Arabic version of Javascript. Why ?',
+      canonicalUrl: '',
+      description: 'Coming Soon',
+      metaImg: '',
+    },
     title: 'An Arabic version of Javascript. Why ?',
-    canonicalUrl: '',
-    description: 'Coming Soon',
-    metaImg: '',
+    speakersTitle: 'About the speakers',
+    backLink: '/talks/',
+    language: 'Arabic',
+    time: '08:00 am -	08:30 am',
+    talkType: '',
+    description: [
+      {
+        desc: 'Coming Soon',
+      },
+    ],
+    multipleSpeakers: 'Speakers',
+    speakersList: [
+      {
+        speakersUrl: '/speakers/burawi-ben-ammar/',
+        speakersName: 'Burawi Ben Ammar',
+        speakersImg: burawi,
+        speakersBio: 'He is a developer that companies call senior because he has experience of 8 years in web development and a Node.js certification from OpenJS - The Linux Foundation.',
+        speakersDesignation: (
+          <span>
+            An Arabic version of Javascript. Why ?
+            <br />
+            Senior Full Stack Developer at Think-it
+          </span>
+        ),
+      },
+    ],
   },
-  title: 'An Arabic version of Javascript. Why ?',
-  speakersTitle: 'About the speakers',
-  backLink: '/talks/',
-  language: 'English',
-  time: '08:00 am -	08:30 am',
-  talkType: 'Fireside Chat',
-  description: [
-    {
-      desc: 'Coming Soon',
+  {
+    url: '/talks/enhanced-developer-experience-with-nx/',
+    metaTags: {
+      title: 'Enhanced developer experience with Nx.',
+      canonicalUrl: '',
+      description: 'Coming Soon',
+      metaImg: '',
     },
-  ],
-  multipleSpeakers: 'Speakers',
-  speakersList: [
-    {
-      speakersUrl: '/speakers/burawi-ben-ammar/',
-      speakersName: 'Burawi Ben Ammar',
-      speakersImg: burawi,
-      speakersBio: 'He is a developer that companies call senior because he has experience of 8 years in web development and a Node.js certification from OpenJS - The Linux Foundation.',
-      speakersDesignation: (
-        <span>
-        An Arabic version of Javascript. Why ?
-        <br />
-        Senior Full Stack Developer at Think-it
-      </span>
-      ),
-    },
-  ],
-},
-{
-  url: '/talks/enhanced-developer-experience-with-nx/',
-  metaTags: {
     title: 'Enhanced developer experience with Nx.',
-    canonicalUrl: '',
-    description: 'Coming Soon',
-    metaImg: '',
+    speakersTitle: 'About the speakers',
+    backLink: '/talks/',
+    language: 'Arabic',
+    time: '08:00 am -	08:30 am',
+    talkType: '',
+    description: [
+      {
+        desc: 'Coming Soon',
+      },
+    ],
+    multipleSpeakers: 'Speakers',
+    speakersList: [
+      {
+        speakersUrl: '/speakers/haykel-ben-jemia/',
+        speakersName: 'Haykel Ben Jemia',
+        speakersImg: haykel,
+        speakersBio: 'He wrote his first line of code at 15 years old using BASIC and never stopped writing code since then. He is still passionate about software development, more specifically developing full stack web applications using JavaScript and serverless technologies.',
+        speakersDesignation: (
+          <span>
+            Enhanced developer experience with Nx.
+            <br />
+            CTO at xTECH
+          </span>
+        ),
+      },
+    ],
   },
-  title: 'Enhanced developer experience with Nx.',
-  speakersTitle: 'About the speakers',
-  backLink: '/talks/',
-  language: 'English',
-  time: '08:00 am -	08:30 am',
-  talkType: 'Fireside Chat',
-  description: [
-    {
-      desc: 'Coming Soon',
+  {
+    url: '/talks/kubernetes-runtime-security-using-falco/',
+    metaTags: {
+      title: 'Kubernetes Runtime Security using Falco.',
+      canonicalUrl: '',
+      description: 'Coming Soon',
+      metaImg: '',
     },
-  ],
-  multipleSpeakers: 'Speakers',
-  speakersList: [
-    {
-      speakersUrl: '/speakers/haykel-ben-jemia/',
-      speakersName: 'Haykel Ben Jemia',
-      speakersImg: haykel,
-      speakersBio: 'He wrote his first line of code at 15 years old using BASIC and never stopped writing code since then. He is still passionate about software development, more specifically developing full stack web applications using JavaScript and serverless technologies.',
-      speakersDesignation: (
-        <span>
-        Enhanced developer experience with Nx.
-        <br />
-        CTO at xTECH
-      </span>
-      ),
-    },
-  ],
-},
-{
-  url: '/talks/kubernetes-runtime-security-using-falco/',
-  metaTags: {
     title: 'Kubernetes Runtime Security using Falco.',
-    canonicalUrl: '',
-    description: 'Coming Soon',
-    metaImg: '',
+    speakersTitle: 'About the speakers',
+    backLink: '/talks/',
+    language: 'Arabic',
+    time: '08:00 am -	08:30 am',
+    talkType: '',
+    description: [
+      {
+        desc: 'Coming Soon',
+      },
+    ],
+    multipleSpeakers: 'Speakers',
+    speakersList: [
+      {
+        speakersUrl: '/speakers/walid-dridi/',
+        speakersName: 'Walid Dridi',
+        speakersImg: walid,
+        speakersBio: 'He is a Kubernetes Consultant/Trainer and CKAD/CKA/CKS Certified. He is interested in cloud-native and software engineering techniques applied to operations.',
+        speakersDesignation: (
+          <span>
+            Kubernetes Runtime Security using Falco.
+            <br />
+            DevOps/SRE at Radio France
+          </span>
+        ),
+      },
+    ],
   },
-  title: 'Kubernetes Runtime Security using Falco.',
-  speakersTitle: 'About the speakers',
-  backLink: '/talks/',
-  language: 'English',
-  time: '08:00 am -	08:30 am',
-  talkType: 'Chat',
-  description: [
-    {
-      desc: 'Coming Soon',
+  {
+    url: '/talks/solid-principles-between-theory-and-practice-in-javaScript/',
+    metaTags: {
+      title: 'SOLID principles between theory and practice in JavaScript.',
+      canonicalUrl: '',
+      description: 'Coming Soon',
+      metaImg: '',
     },
-  ],
-  multipleSpeakers: 'Speakers',
-  speakersList: [
-    {
-      speakersUrl: '/speakers/walid-dridi/',
-      speakersName: 'Walid Dridi',
-      speakersImg: walid,
-      speakersBio: 'He is a Kubernetes Consultant/Trainer and CKAD/CKA/CKS Certified. He is interested in cloud-native and software engineering techniques applied to operations.',
-      speakersDesignation: (
-        <span>
-        Kubernetes Runtime Security using Falco.
-        <br />
-        DevOps/SRE at Radio France
-      </span>
-      ),
-    },
-  ],
-},
-{
-  url: '/talks/solid-principles-between-theory-and-practice-in-javaScript/',
-  metaTags: {
     title: 'SOLID principles between theory and practice in JavaScript.',
-    canonicalUrl: '',
-    description: 'Coming Soon',
-    metaImg: '',
+    speakersTitle: 'About the speakers',
+    backLink: '/talks/',
+    language: 'Arabic',
+    time: '08:00 am -	08:30 am',
+    talkType: '',
+    description: [
+      {
+        desc: 'Coming Soon',
+      },
+    ],
+    multipleSpeakers: 'Speakers',
+    speakersList: [
+      {
+        speakersUrl: '/speakers/nacer-nsiri/',
+        speakersName: 'Nacer Nsiri',
+        speakersImg: nacer,
+        speakersBio: 'He has held various positions across his 7 years career such as Technical Lead and Senior Software engineer Frontend, Backend, and Fullstack, has a Node.js certification from OpenJS - The Linux Foundation, always sharing knowledge and assisting coworkers, always looking for challenges, striving for continuous self-improvement.',
+        speakersDesignation: (
+          <span>
+            SOLID principles between theory and practice in JavaScript.
+            <br />
+            Senior FrontEnd Engineer at Oodrive
+          </span>
+        ),
+      },
+    ],
   },
-  title: 'SOLID principles between theory and practice in JavaScript.',
-  speakersTitle: 'About the speakers',
-  backLink: '/talks/',
-  language: 'English',
-  time: '08:00 am -	08:30 am',
-  talkType: 'Fireside Chat',
-  description: [
-    {
-      desc: 'Coming Soon',
-    },
-  ],
-  multipleSpeakers: 'Speakers',
-  speakersList: [
-    {
-      speakersUrl: '/speakers/nacer-nsiri/',
-      speakersName: 'Nacer Nsiri',
-      speakersImg: nacer,
-      speakersBio: 'He has held various positions across his 7 years career such as Technical Lead and Senior Software engineer Frontend, Backend, and Fullstack, has a Node.js certification from OpenJS - The Linux Foundation, always sharing knowledge and assisting coworkers, always looking for challenges, striving for continuous self-improvement.',
-      speakersDesignation: (
-        <span>
-        SOLID principles between theory and practice in JavaScript.
-        <br />
-        Senior FrontEnd Engineer at Oodrive
-      </span>
-      ),
-    },
-  ],
-},
 ];
 
 
@@ -337,7 +337,7 @@ const scheduleDetailsTwo = [
     title: 'Day 2 Begins',
     time: '07:55 am',
   },
-  
+
   {
     url: '/talks/enhanced-developer-experience-with-nx/',
     type: 'talk',
