3e2a3504172795a26733c4ec773e7ac433d5892a
3imed-jaberi <imed_jebari@hotmail.fr>
2021-12-04 22:50:11 +0100

    sort talk by speaker name

--- src/components/Homepage/Talks.js
+++ src/components/Homepage/Talks.js
@@ -1,42 +1,31 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react';
 import { Link } from 'gatsby';
 import '../Common/Styles.scss';
 import IndivTalk from './IndivTalk';
-import { talkDetailsPage } from '../Homepage/AllState.js'
+import { talkDetailsPage } from '../Homepage/AllState.js';
 
-const Talks = props => {
+const Talks = (props) => {
   return (
-    <div id='talks' className={'sectionWrapper removePaddBottom ' + ((props.wdClass) ? ' wd80' : '')}>
-      <div className='workshopWrapper'>
-        <div className='articleSubTitle'>Talks</div>
+    <div id="talks" className={'sectionWrapper removePaddBottom ' + (props.wdClass ? ' wd80' : '')}>
+      <div className="workshopWrapper">
+        <div className="articleSubTitle">Talks</div>
         <ul>
-          {
-            talkDetailsPage.sort((a, b) => a.title > b.title ? 1 : -1)
-              .map((talksList, index) => {
-                return (
-                  <Fragment key={index}>
-                    {
-                      !talksList.isWorkshop ? (
-                        <Fragment>
-                          {
-                            index < 6 ? (
-                              <IndivTalk talksList={talksList} />
-                            ) : null
-                          }
-                        </Fragment>
-                      ) : null
-                    }
-                  </Fragment>
-                )
-              })
-          }
+          {talkDetailsPage
+            .sort((a, b) => (a.speakersList.speakersName > b.speakersList.speakersName ? 1 : -1))
+            .map((talksList, index) => {
+              return (
+                <Fragment key={index}>
+                  {!talksList.isWorkshop ? (
+                    <Fragment>{index < 6 ? <IndivTalk talksList={talksList} /> : null}</Fragment>
+                  ) : null}
+                </Fragment>
+              );
+            })}
         </ul>
-        <div className='backBtnWrapper'>
-          <div className={((!props.isLightMode) ? 'lightLine' : 'darkLine')}></div>
-          <Link to='/talks/'>
-            <button className='commonBtn darkBtn'>
-              View all Talks
-            </button>
+        <div className="backBtnWrapper">
+          <div className={!props.isLightMode ? 'lightLine' : 'darkLine'}></div>
+          <Link to="/talks/">
+            <button className="commonBtn darkBtn">View all Talks</button>
           </Link>
         </div>
       </div>
